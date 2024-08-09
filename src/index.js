c184f4d6aec625d6fd7b4868cc7aa8fb27159454
Lorena Hurtado <hilorena.contact@gmail.com>
2024-08-09 16:54:13 +0100

    get temperature data from api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    // API call and update the UI
+    let apiKey = "89c0384tf17ebb5abc361e3ao1434ea8";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
 
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
